69ad4db0e2731256e157ffb4ed7da7955904ad50
alexandrac15 <46963017+alexandrac15@users.noreply.github.com>
2020-08-06 14:44:54 +0300

    Add files via upload
    Script that gets the historical data for a company and saves it in csv.

---
+++ aquisition.py
@@ -0,0 +1,35 @@
+#Get data from API: 
+
+#INPUT:  'symbol'
+#OUTPUT: ---
+import requests
+import sys
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+
+def getData(symbol):
+    
+    token="https://www.youtube.com/watch?v=jhBRfsrBTdo"
+    URL= "https://cloud.iexapis.com/v1/stock/"+symbol+"/chart/max?token="+token;
+    #request the data from provider
+    r = requests.get(url = URL);
+   
+    data = r.json() 
+    #put the data in a dataframe
+    df=pd.json_normalize(data);
+
+    #make sure the columns in the following order
+    dffile=df[[	'date',	'uClose','uOpen','uHigh','uLow','uVolume','close','open','high','low','volume','currency','change','changePercent','label','changeOverTime']].copy()
+    #save data frame to the   correspoding cvs file
+    dffile.to_csv('C:\\Users\\aalex\\Stocks Project\\historic_data\\'+symbol+'.csv'); 
+  
+   
+
+if __name__ == "__main__":
+    arguments= sys.argv;
+    getData(sys.argv[1]);
+
+    
+    
+    
